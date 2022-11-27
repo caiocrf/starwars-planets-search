4eae51bf3b4abb5de49c1fd1e99d7359c309d4a6
Caio Pinho <icandoit2026@gmail.com>
2022-11-27 20:04:50 -0300

    req 3 e 4

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -5,6 +5,9 @@ function Table() {
   const [planet, setPlanet] = useState([]);
   const [filterPlanetName, setFilterPlanetName] = useState([]);
   const [namePlanet, setnamePlanet] = useState('');
+  const [column, setColumn] = useState('population');
+  const [operator, setOperator] = useState('maior que');
+  const [quantity, setQuantity] = useState(0);
 
   const getApi = async () => {
     setPlanet(await planetListAPI());
@@ -28,10 +31,61 @@ function Table() {
     }
   };
 
+  const columnOpition = ({ target: { value } }) => {
+    setColumn(value);
+  };
+
+  const operatorOpition = ({ target: { value } }) => {
+    setOperator(value);
+  };
+
+  const onChangeQuantity = ({ target: { value } }) => {
+    setQuantity(value);
+  };
+
+  const filterHandleChange = () => {
+    if (operator === 'maior que') {
+      const filter = filterPlanetName.filter((e) => +e[column] > quantity);
+      return setFilterPlanetName(filter);
+    }
+    if (operator === 'menor que') {
+      const filter = filterPlanetName.filter((e) => +e[column] < quantity);
+      return setFilterPlanetName(filter);
+    }
+    if (operator === 'igual a') {
+      const filter = filterPlanetName.filter((e) => e[column] === quantity);
+      return setFilterPlanetName(filter);
+    }
+  };
   return (
     <div>
       <form>
         <input type="text" data-testid="name-filter" onChange={ getName } />
+        <select data-testid="column-filter" onClick={ columnOpition }>
+          <option value="population">population</option>
+          <option value="orbital_period">orbital_period</option>
+          <option value="diameter">diameter</option>
+          <option value="rotation_period">rotation_period</option>
+          <option value="surface_water">surface_water</option>
+        </select>
+        <select data-testid="comparison-filter" onClick={ operatorOpition }>
+          <option value="maior que">maior que</option>
+          <option value="menor que">menor que</option>
+          <option value="igual a">igual a</option>
+        </select>
+        <input
+          data-testid="value-filter"
+          onChange={ onChangeQuantity }
+          value={ quantity }
+          type="number"
+        />
+        <button
+          data-testid="button-filter"
+          type="button"
+          onClick={ filterHandleChange }
+        >
+          Filter
+        </button>
       </form>
       <table>
         <tr>
